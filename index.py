3409cd956a0c592fbdfef027fa4b32cf2e196163
Gurveer Madurai <maduraigurveer@gmail.com>
2024-01-25 21:44:25 -0800

    model prediction tested with success

--- index.py
+++ index.py
@@ -65,3 +65,22 @@ test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
 print('Accuracy on test data: ', accuracy_score(X_test_prediction, Y_test)) #outcome of ~80%
 
+# building a predictive system 
+
+# input_data = (56,1,1,120,236,0,1,178,0,0.8,2,0,2) ## remove the comment on this line to test for an individual with heart disease([1])
+input_data = (60,1,0,130,206,0,0,132,1,2.4,1,2,3)
+# change input data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+# print(input_data_as_numpy_array)
+
+# reshape numpy array as we are predicting for only one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = model.predict(input_data_reshaped)
+print(prediction)
+
+if(prediction[0]==0):
+    print('This individual does not have a heart disease')
+else:
+    print('This individual has heart disease')
+
